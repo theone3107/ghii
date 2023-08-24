23412853a51c2fa417eac3c80beac94433ca77f5
Pratham saxena <128896771+theone3107@users.noreply.github.com>
2023-08-24 07:37:34 +0000

    hihi

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"Use strict";
+
+//alert( 3 + 3 )// we are using nodejs not the browser so it will not work
+
+console.log(60+9)
+console.log("Pratham");
+let name = "Pratham" 
+let age = 20 
+let isloggedIn = false
+console.log(typeof age);
+// string in double quotes
+// number =< 2 ki power 53
+ // boolean = true ya false
+// bigInt
+// null = standalone value
+// undefined = 
+// symbol = for uniqueness, will be used in REACT
+// undefined type = undefined 
+// null type = object
