2fa0d70d23dbecb7aaa50b7d9357edba15beb436
Pratham saxena <128896771+theone3107@users.noreply.github.com>
2023-08-24 08:02:40 +0000

    conversionnn

---
+++ basics/conversion.js
@@ -0,0 +1,15 @@
+// About Coversionssssss
+// here = conversion
+// "33" was string = 33 Number
+// "33abc" = NaN (Not a Number)
+// true = 1 ; false = 0 vice versa
+// "" = false; "abc" = true
+let score = "69"
+let valueinNumber = Number(score)
+console.log(valueinNumber);
+
+let isloggedIn = 1;
+
+let booleanisloggedIn = Boolean(isloggedIn);
+console.log(booleanisloggedIn);
+console.log(typeof valueinNumber);
