f38a87412b894f7983d847d62e231dc0056d2770
Pratham saxena <128896771+theone3107@users.noreply.github.com>
2023-08-24 07:10:59 +0000

    contain config

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 3107
+let accountemail = "Pratham@mail.com"
+accountcity = "tokyo"
+console.log("Pratham");
+console.log(accountId);
+accountemail = "new@mail.coim"
+accountcity = "Hyderabad"
+console.log(accountemail);
+console.log(accountcity);
+
