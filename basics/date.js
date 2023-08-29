a3802bc7ff0d161902f07e68d46e2d11991fc1dc
Pratham saxena <128896771+theone3107@users.noreply.github.com>
2023-08-29 09:30:51 +0000

    d

---
+++ basics/date.js
@@ -0,0 +1,6 @@
+// mydate= new Date()
+//console.log(mydate.toString());
+//console.log(mydate.toDateString);
+let mycreatedate=new Date("09-02-2002")
+console.log(mycreatedate.toString());
+console.log(mycreatedate.toLocaleDateString);
